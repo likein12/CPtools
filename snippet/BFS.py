f0cee2f01046d95f0a7e1de8002a251206c666c5
likein12 <deoxyglucose3@gmail.com>
2020-11-22 23:47:42 +0900

    modified BFS

--- snippet/BFS.py
+++ snippet/BFS.py
@@ -5,16 +5,12 @@ INF = float('inf')
 
 Q = deque([vi])
 
-checked_list = [False]*N
-checked_list[vi]=True
-
 min_path_list = [INF]*N #change
 min_path_list[vi] = 0
 
 while len(Q)>0:
     v = Q.pop()
     for v1 in e_list[v]:
-        if not checked_list[v1]:
-            checked_list[v1]=True
+        if min_path_list[v1] >= INF:
             Q.appendleft(v1)
-            min_path_list[v1]=min(min_path_list[v1],min_path_list[v]+1) #change
+            min_path_list[v1] = min_path_list[v]+1 #change
